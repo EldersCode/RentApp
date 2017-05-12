f7c8f85e5d9119776317f1942b38112c4cc4f126
Morsy Mohamed Elsokary <morsysoker@hotmail.com>
2017-05-12 13:21:26 +0200

    events + showyet t3delat

--- app/src/main/java/com/tourism/hesham/rentapp/EventsActivity.java
+++ app/src/main/java/com/tourism/hesham/rentapp/EventsActivity.java
@@ -3,13 +3,35 @@ package com.tourism.hesham.rentapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventsActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+
+
+        List<String> list = new ArrayList<String>();
+        list.add("Item 1");
+        list.add("Item 2");
+        list.add("Item 3");
+        list.add("Item 4");
+        list.add("Item 5");
+        adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
